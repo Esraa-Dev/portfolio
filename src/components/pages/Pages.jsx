054e2b8a238b98a72ca2acd6c04f29c56976fc18
EsraaMohammadAbdelmoteleb <79087134+EsraaMohammadAbdelmoteleb@users.noreply.github.com>
2022-08-30 06:26:22 +0200

    second commit

---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "../common/Footer";
+import  Header  from "../common/Header";
+import { Home } from "../home/Home";
+import { About } from "./About";
+import { Blog } from "./Blog";
+import { Contact } from "./Contact";
+import { Portfolio } from "./Portfolio";
+import { Services } from "./Services";
+import { Testimonials } from "./Testimonials";
+const Pages = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Pages
